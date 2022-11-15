dff829983092a980b4ca2d3577e099482d9ce0dc
pat <patipat@sage.art>
2022-11-15 10:34:57 -0500

    initial commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+require('plugins')
+require('settings')
+require('maps')
+require('twopats-lsp')
+require('twopats-treesitter')
+require("interface")
+
+--misc set ups
+vim.cmd('colorscheme gruvbox')
+require('nvim_comment').setup()
---
+++ lua/twopats-lsp.lua
@@ -0,0 +1,86 @@
+local lspconfig = require'lspconfig'
+--
+lspconfig.emmet_ls.setup{
+filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "astro" }
+}
+lspconfig.tsserver.setup{}
+lspconfig.astro.setup{
+}
+lspconfig.solc.setup{
+   root_dir = lspconfig.util.root_pattern('foundry.*', '.git'),
+}
+lspconfig.bashls.setup{}
+lspconfig.solidity_ls.setup{}
+lspconfig.gopls.setup{}
+lspconfig.eslint.setup{}
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+--auto complete
+---- set up autocomplete
+
+-- require("nvim-lsp-installer").setup({
+-- 		ensure_installed = {"sumneko_lua", "solang", "prismals","tsserver", "eslint", "emmet_ls"},
+--     automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+--     ui = {
+--         icons = {
+--             server_installed = "✓",
+--             server_pending = "➜",
+--             server_uninstalled = "✗"
+--         }
+--     }
+-- })
+
+local cmp = require('cmp')
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		-- { name = 'vsnip' }, -- For vsnip users.
+		{ name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
